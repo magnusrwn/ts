756e7913df3d332d1fd5a9f5ac1f5a4d7d59f4fe
magnusrwn <119822862+magnusrwn@users.noreply.github.com>
2025-07-22 13:51:25 +0100

    Classes and inheretence of methods, set, and get

--- setup_for_ts/src/main.ts
+++ setup_for_ts/src/main.ts
@@ -9,3 +9,59 @@
 
 console.log('x')
 //('tsc -w' whips this into the js file)
+
+
+// Calsses
+// you must define the inps to the function before youdefine them as inputs... if that makes sense...
+class User {
+    email: string
+    name: string
+    friends: string[] = []
+    constructor(email:string, name:string){
+        this.email = email,
+        this.name = name
+    }
+}
+
+const mag = new User("x@wmail.com", "mag")
+mag.friends
+
+
+// you can make things public or private. Private means you can not access it ever again throguh dot notation, but it is saved for use within the class only. Ex:
+class UserPriv {
+    name: string
+    username: number
+    private readonly dbId: number = 12 // this is un seeable, and unedutable.
+    constructor (name:string, username:number){{
+        this.name = name,
+        this.username = username
+    }}
+    // you can make 'getters', these are mini functions that return vals that are in the class
+    get userData(): User{
+        return {email:"email", name:"mag", friends:["x"] }
+    }
+    private countNum: number = 1 
+    set setCountForSomething(countNum: number) {
+        if (countNum > 1) {
+            throw new Error("err")
+        } else {
+            this.countNum = countNum
+        }
+    }
+    // following is a method thats good to use in this class only
+    protected deleteToken():void{
+        console.log(`token deleted`)
+    }
+}
+const magn = new UserPriv("name", 123)
+
+
+
+// getters and setters are essentially jsut things to get and set values in classes with conditionals.
+
+
+// to inherit classes you can use 'extends' as well, as shown:
+class AnotherUser extends UserPriv {
+    isMasterUser: boolean = true
+    //deleteToken:() // this cant be used when set to private in the parent, and it also has to be called in a function, as currently, this is just re-defining it
+}
